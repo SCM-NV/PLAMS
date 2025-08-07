db0a476b9409c443257d0f2c8e5b9b482b5f896e
Franco Egidi <egidi@scm.com>
2025-08-07 17:18:05 +0200

    SO101: failed black, fixed

--- examples/VCDSpectrumADF/moxy_vcd.py
+++ examples/VCDSpectrumADF/moxy_vcd.py
@@ -1,6 +1,7 @@
 from scm.plams import AMSJob, Molecule
 from scm.input_classes import drivers, engines
-#import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 # This is a very simple script for the calculation of the VCD
 # spectrum of (R)-methyloxirane using ADF at the PBE/DZP level
@@ -10,22 +11,23 @@ from scm.input_classes import drivers, engines
 
 # To plot the spectrum you can uncomment all lines related to matplotlib
 
+
 def main():
     """
     The main script
     """
-    
+
     # Define molecule, here we use a file with a pre-optimized geometry
     # Note: the geometry has been optimized at the same level of theory
     #       that we are now using for the VCD spectrum!!!
     methyloxirane = Molecule("R-methyloxirane.xyz")
-    
+
     # Define engine
     driver = drivers.AMS()
     driver.Task = "SinglePoint"
     driver.Properties.VCD = "Yes"
     driver.Engine = engines.ADF()
-    
+
     # Define settings
     driver.Engine.Basis.Core = "None"
     driver.Engine.Basis.Type = "DZP"
@@ -34,13 +36,13 @@ def main():
     # Run job
     job = AMSJob(molecule=methyloxirane, settings=driver, name="moxy_vcd")
     results = job.run()
-    
+
     # Collect results, first we get the rotational strengths
     R = results.get_vcd_rotational_strength()
-    print(f'VCD rotational strengths in (10-44 esu^2 cm^2):\n {R}')
+    print(f"VCD rotational strengths in (10-44 esu^2 cm^2):\n {R}")
     # Then we print the rotational strength convoluted spectrum
     spectrum = results.get_vcd_spectrum()
-    print(f'VCD rotational strengths spectrum:\n {spectrum}')
+    print(f"VCD rotational strengths spectrum:\n {spectrum}")
     # Optionally plot the spectrum
     # x_freq, y_intens_raw = spectrum
     # plt.plot(x_freq, y_intens_raw)
@@ -49,6 +51,6 @@ def main():
     # plt.xlim(500, 4000)
     # plt.show()
 
+
 if __name__ == "__main__":
     main()
-
