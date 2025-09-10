78d20b2f32c0c00b7154e3ef57a2820d0cce0417
Paul Spiering <spiering@scm.com>
2025-09-10 10:36:28 +0200

    Replace KFFile.read with typed versions in amsanalysis.py

--- src/scm/plams/interfaces/adfsuite/amsanalysis.py
+++ src/scm/plams/interfaces/adfsuite/amsanalysis.py
@@ -65,19 +65,19 @@ class AMSAnalysisPlot:
         xnums = sorted([xnum for xnum in set(xnums)])
         for i in xnums:
             xkey = f"x({i})-axis"
-            self.x.append(kf.read(sec, xkey))
-            x_name: str = kf.read(sec, f"{xkey}(label)")
+            self.x.append(kf.read_reals(sec, xkey))
+            x_name = kf.read_string(sec, f"{xkey}(label)")
             self.x_names.append(convert_to_unicode(x_name))
-            self.x_units.append(convert_to_unicode(kf.read(sec, f"{xkey}(units)")))
+            self.x_units.append(convert_to_unicode(kf.read_string(sec, f"{xkey}(units)")))
 
         # Read the y-values
         ykey = "y-axis"
-        y_name = kf.read(sec, f"{ykey}(label)")
-        self.y = kf.read(sec, ykey)
+        y_name = kf.read_string(sec, f"{ykey}(label)")
+        self.y = kf.read_reals(sec, ykey)
         self.y_name = convert_to_unicode(y_name)
-        self.y_units = convert_to_unicode(kf.read(sec, f"{ykey}(units)"))
+        self.y_units = convert_to_unicode(kf.read_string(sec, f"{ykey}(units)"))
 
-        self.y_sigma = kf.read(sec, "sigma")
+        self.y_sigma = kf.read_reals(sec, "sigma")
 
         self.read_properties(kf, sec)
         self.section = sec.split("(")[0] + "_" + sec.split("(")[1].split(")")[0]
@@ -92,7 +92,7 @@ class AMSAnalysisPlot:
         while 1:
             counter += 1
             try:
-                propname = kf.read(sec, f"Property({counter})").strip()
+                propname = kf.read_string(sec, f"Property({counter})").strip()
             except:
                 break
             properties[propname] = kf.read(sec, propname)
@@ -252,7 +252,7 @@ class AMSAnalysisResults(SCMResults):
 
         Extract user input from the kf file and parse it back to a |Settings| instance using ``scm.libbase`` module. Remove the ``system`` branch from that instance.
         """
-        user_input: str = self._kf.read("General", "user input")
+        user_input = self._kf.read_string("General", "user input")
         try:
             inp = input_to_settings(user_input, program="analysis")
         except:
