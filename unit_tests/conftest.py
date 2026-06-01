9cb11b956648c38cdec4c8b318f0ba1034fa92e6
David Ormrod Morley <ormrodmorley@scm.com>
2026-06-01 17:26:41 +0200

    Add x-platform pickle test SO--

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -68,3 +68,13 @@ def coskf_folder():
     p = Path(__file__).parent.absolute() / "coskf"
     assert p.exists()
     return p
+
+
+@pytest.fixture(scope="session")
+def dill_folder():
+    """
+    Returns the path to the dill fixture folder.
+    """
+    p = Path(__file__).parent.absolute() / "dill"
+    assert p.exists()
+    return p
